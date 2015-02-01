a8fb9117cd58168aa78a7cf22c2ade0d39826039
Sylvain Hugues-Laclef <info@tapiocadesign.com>
2015-02-01 17:32:03 +0100

    var speard everywhere thanks to factory method

--- angular/js/app.js
+++ angular/js/app.js
@@ -7,24 +7,35 @@ myApp.config(["$routeProvider", function($routeProvider) {
 	//console.log("myApp.config");
 	$routeProvider.
 	when("/concerts-list", {
-		templateUrl: "views/concerts-list.html"
+		templateUrl: "views/concerts-list.html",
+		controller:"ConcertsListController"
 	}).
 	when("/concerts-map", {
-		templateUrl: "views/concerts-map.html"
+		templateUrl: "views/concerts-map.html",
+		controller:""
 	}).
 	when("/concert/:workId", {
-		templateUrl: "views/concert.html"
+		templateUrl: "views/concert.html",
+		controller:"ConcertController"
 	}).
 	when("/places-list", {
-		templateUrl: "views/places-list.html"
+		templateUrl: "views/places-list.html",
+		controller:"PlacesListController"
 	}).
 	when("/place/:placeId", {
-		templateUrl: "views/place.html"
+		templateUrl: "views/place.html",
+		controller:"PlaceController"
 	}).
 	otherwise({
-		redirectTo: "/404"
+		redirectTo: "/concerts-list"
 	});
 }]);
+
+
+myApp.factory('Data', function () {
+    return { FirstName: 'Zazanette' };
+});
+
 
 
 /*
@@ -35,7 +46,15 @@ var concerts = $resource(
 console.log(concerts);
 }]);
 */
-myApp.controller('ConcertsListController', ['$scope','$http', function($scope,$http) {
+
+
+
+
+
+
+myApp.controller('ConcertsListController', ['$scope','$http', 'Data', function($scope,$http, Data) {
+      $scope.Data = Data;
+      $scope.header_title = "111";
       $http.get('http://localhost/symfony/web/app_dev.php/api/city/1/worksOrderedByFirstPerformance').
         success(function(concerts) {
             console.log(concerts);
@@ -43,8 +62,9 @@ myApp.controller('ConcertsListController', ['$scope','$http', function($scope,$h
         });
  }]);
 myApp.controller('ConcertController', ['$scope','$http', '$routeParams', function($scope,$http, $routeParams) {
+	$scope.header_title = "222";
 	var workId = $routeParams.workId;
-	console.log("workId="+workId);
+	//console.log("workId="+workId);
     $http.get('http://localhost/symfony/web/app_dev.php/api/work/'+workId).
         success(function(work) {
             $scope.work = work;
@@ -53,16 +73,28 @@ myApp.controller('ConcertController', ['$scope','$http', '$routeParams', functio
 
 
 
+myApp.controller('SecondCtrl', ['$rootScope','$scope','$http', 'Data', function($rootScope, $scope,$http, Data) {
+      $scope.Data = Data;
+}]);
+myApp.controller('ThirdCtrl', ['$rootScope','$scope','$http', 'Data', function($rootScope, $scope,$http, Data) {
+      $scope.Data = Data;
+}]);
 
 
-myApp.controller('PlacesListController', ['$scope','$http', function($scope,$http) {
+
+
+myApp.controller('PlacesListController', ['$rootScope','$scope','$http', function($rootScope, $scope,$http) {
+      $rootScope.header_title = "333";
       $http.get('http://localhost/symfony/web/app_dev.php/api/city/1/places').
         success(function(places) {
             console.log(places);
             $scope.places = places;
         });
  }]);
-myApp.controller('PlaceController', ['$scope','$http', '$routeParams', function($scope,$http, $routeParams) {
+myApp.controller('PlaceController', ['$rootScope','$scope','$http', '$routeParams', function($rootScope, $scope,$http, $routeParams) {
+	$rootScope.header_title_over = "444";
+	console.log("PC $rootScope.header_title_over = "+$rootScope.header_title_over);
+    
 	var placeId = $routeParams.placeId;
     $http.get('http://localhost/symfony/web/app_dev.php/api/place/'+placeId).
         success(function(place) {
@@ -81,11 +113,17 @@ myApp.controller('PlaceController', ['$scope','$http', '$routeParams', function(
  	};
  }]);
 
+myApp.controller('HeaderController', ['$rootScope','$scope','$http','Data', function($rootScope, $scope,$http, Data) {
+      $scope.Data = Data;
+      console.log("HC $rootScope.header_title_over = "+$rootScope.header_title_over);
+      $scope.header_title = $rootScope.header_title_over;
+}]);
+
+
 
 
 myApp.run(function($rootScope) {
-	
-	console.log("ANGULAR DEBUG");
+	//console.log("ANGULAR DEBUG");
 	$rootScope.$on('$stateChangeStart',function(event, toState, toParams, fromState, fromParams){
   console.log('$stateChangeStart to '+toState.to+'- fired when the transition begins. toState,toParams : \n',toState, toParams);
 });
