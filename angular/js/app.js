c514151932c58ec928a2c98e5d7f51e9256205c6
Sylvain Hugues-Laclef <info@tapiocadesign.com>
2015-02-06 14:49:53 +0100

    about to resolve concerts list, ul is duplicated as much as li

--- angular/js/app.js
+++ angular/js/app.js
@@ -31,20 +31,24 @@ myApp.config(["$routeProvider", function($routeProvider) {
 	});
 }]);
 
-
-/*
-myApp.directive('headerSideBtnRight', function() {
-  return {
-      restrict: 'C',
-      template: '<a href="#" class="white header-side-btn" header-side-btn-right><span class="glyphicon glyphicon-menu-hamburger red-dark"></span></a>'
-  };
-});
-*/
-
 myApp.factory('Data', function () {
     return { headerTitle: '', searchTerm:"" };
 });
 
+
+
+// app.directive('main', ['$rootScope','$location', function ($rootScope, $location) {
+//    return {
+//        restrict: 'E',
+//        scope: false,
+//        link: function postLink(scope, element, attrs) {
+//            console.log('All directive elements execute this!');
+//            $rootScope.$on('$routeChangeStart', function() {
+//                console.log('ng-included elements work execute this!');
+//            });
+//        }
+//     }
+// }]);
 
 
 // myApp.directive('headerSideBtnLeft', function() {
@@ -67,17 +71,19 @@ myApp.controller('HeaderController', ['$rootScope','$scope','$http',"$route", 'D
 
       //$scope.obj.val = "caca";
 
-
-
-
-
-
       $scope.$on('$routeChangeStart', function(next, current) { 
+
+      // console.log("Data.headerTitle");
+      // console.log(Data.headerTitle);
+      // console.log(Data.headerTitle.length);
+
           var currentViewController = current.$$route.controller;
           console.log("currentViewController");
           console.log(currentViewController);
+
            $scope.search={};
            $scope.back={};
+           
           if (currentViewController=="ConcertsListController" || currentViewController=="PlacesListController")  {
               $scope.search.visible = 1;
               $scope.back.visible = 0;
@@ -159,7 +165,39 @@ myApp.controller('PlaceController', ['$rootScope','$scope','$http', '$routeParam
 
 
 
+// myApp.controller('FooterController', ['$rootScope','$scope','$http',"$route", 'Data', function($rootScope, $scope, $http, $route, Data) {
+//       $scope.$on('$routeChangeStart', function(next, current) { 
 
+//           var currentViewController = current.$$route.controller;
+//           // console.log("currentViewController");
+//           // console.log(currentViewController);
+//            $scope.listBtn={};
+//            $scope.mapBtn={};
+//            $scope.Btn={};
+           
+//           if (currentViewController=="ConcertsListController" || currentViewController=="PlacesListController")  {
+//               $scope.search.visible = 1;
+//               $scope.back.visible = 0;
+//           } else if (currentViewController=="ConcertController")  {
+//               $scope.search.visible = 0;
+//               $scope.back.visible = 1;
+//               $scope.back.url = "#/concerts-list";
+//           } else if (currentViewController=="PlaceController")  {
+//               $scope.search.visible = 0;
+//               $scope.back.visible = 1;
+//               $scope.back.url = "#/places-list";
+//           } else {
+//               $scope.search.visible = 1;
+//               $scope.back.visible = 0;
+//           }
+//       });
+
+//       $scope.Data = Data;
+//       $scope.searchShowHideToggle = function() {
+//           $scope.searchShowHideToggleBoolean = !$scope.searchShowHideToggleBoolean;
+          
+//       };
+// }]);
 
 
 myApp.run(function($rootScope) {
