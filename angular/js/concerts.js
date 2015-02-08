4661e91829ad223cbafbdb295973cbe8745945b6
Sylvain Hugues-Laclef <info@tapiocadesign.com>
2015-02-08 02:45:14 +0100

    about to build rest for place/8/works

--- angular/js/concerts.js
+++ angular/js/concerts.js
@@ -19,7 +19,7 @@ myApp.controller('ConcertsListController', ['$rootScope','$scope','$http', "$rou
  }]);
 
 /***********************************************************************************************/
-myApp.controller('ConcertController', ['$rootScope','$scope','$http', '$routeParams','Data','Search', function($rootScope,$scope,$http, $routeParams, Data, Search) {
+myApp.controller('ConcertController', ['$rootScope','$scope','$http', '$location','$routeParams','Data','Search', function($rootScope,$scope,$http, $location, $routeParams, Data, Search) {
 
 $scope.colors = ["#fc0003", "#f70008", "#f2000d", "#ed0012", "#e80017", "#e3001c", "#de0021", "#d90026", "#d4002b", "#cf0030", "#c90036", "#c4003b", "#bf0040", "#ba0045", "#b5004a", "#b0004f", "#ab0054", "#a60059", "#a1005e", "#9c0063", "#960069", "#91006e", "#8c0073", "#870078", "#82007d", "#7d0082", "#780087", "#73008c", "#6e0091", "#690096", "#63009c", "#5e00a1", "#5900a6", "#5400ab", "#4f00b0", "#4a00b5", "#4500ba", "#4000bf", "#3b00c4", "#3600c9", "#3000cf", "#2b00d4", "#2600d9", "#2100de", "#1c00e3", "#1700e8", "#1200ed", "#0d00f2", "#0800f7", "#0300fc"];
 
@@ -80,10 +80,17 @@ $scope.colors = ["#fc0003", "#f70008", "#f2000d", "#ed0012", "#e80017", "#e3001c
             }
 
 
-$scope.clickWebsite = function() {
+$scope.clickWebsite = function($scope, $location) {
      console.log("clickWebsite");
-  }
+     //$location.path('www.france2.fr');
+      //$scope.$apply(function() { $location.path("http://www.france2.fr"); });
 
+  }
+$scope.clickPlace = function($scope, $location, placeId) {
+     console.log("clickPlace "+placeId);
+     //$location.path('www.france3.fr');
+     
+  }
 
 	var workId = $routeParams.workId;
     $http.get('http://localhost/symfony/web/app_dev.php/api/work/'+workId).
