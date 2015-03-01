fec46dad1a23e0759e885fcb1f7c6876a464d87a
Sylvain Hugues-Laclef <info@tapiocadesign.com>
2015-03-01 02:45:10 +0100

    angular static: if image local is not find, load the server one

---
+++ v1/angular-static/js/footer.js
@@ -0,0 +1,27 @@
+myApp.controller('FooterController', ['$rootScope','$scope','$http',"$route", 'Data', function($rootScope, $scope, $http, $route, Data) {
+     //  $scope.$on('$routeChangeStart', function(next, current) { 
+
+     //      var currentViewController = current.$$route.controller;
+          
+     //      if (currentViewController=="ConcertsListController" || currentViewController=="PlacesListController")  {
+     //          $scope.listClass="";
+			  // $scope.mapClass="";
+     //          $scope.placeClass="";
+
+     //      } else if (currentViewController=="ConcertController")  {
+              
+
+     //      } else if (currentViewController=="PlaceController")  {
+              
+              
+     //      } else {
+     //          console.log("ERROR from footer, url should not end up here");
+     //      }
+     //  });
+
+     //  $scope.Data = Data;
+     //  $scope.searchShowHideToggle = function() {
+     //      $scope.searchShowHideToggleBoolean = !$scope.searchShowHideToggleBoolean;
+          
+     //  };
+}]);
