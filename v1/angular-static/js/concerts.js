cf65505862298dca85462ddd5ca12090c12611a1
Sylvain Hugues-Laclef <info@tapiocadesign.com>
2015-03-01 14:52:18 +0100

    local storage done

--- v1/angular-static/js/concerts.js
+++ v1/angular-static/js/concerts.js
@@ -9,23 +9,46 @@ myApp.controller('ConcertsListController', ['$rootScope','$scope','$http', "$rou
       // $scope.Const = Const;
       //Const.baseUrl = "http://"+$location.host();
 
-      $http.get(Const.baseUrl+'/symfony/web/api/city/1/worksOrderedByFirstPerformance').
-        success(function(concerts) {
-            $scope.concerts = concerts;
-            //should be loadede and display after a few time, so allow no result 
-            Data.loadingActive = 0;
-            // console.log(Data.loadingActive);
-            setTimeout(function(){ Data.concertsNoResultAllowed = 1; }, 2000);
-      }).
-      error(function(data, status, headers, config) {
-          alert("Concerts list http error: "+status);
-      });
+
+
+      // console.log("local storage");
+      //var retrievedObject = localStorage.getItem('worksOrderedByFirstPerformance');
+
+
+      if (localStorage.getItem("city/1/worksOrderedByFirstPerformance") === null) {
+          console.log("city/1/worksOrderedByFirstPerformance not in lStor");
+          $http.get(Const.baseUrl+'/symfony/web/api/city/1/worksOrderedByFirstPerformance').
+            success(function(concerts) {
+                $scope.concerts = concerts;
+
+                localStorage.setItem('city/1/worksOrderedByFirstPerformance', JSON.stringify(concerts));
+
+                //should be loadede and display after a few time, so allow no result 
+                Data.loadingActive = 0;
+                // console.log(Data.loadingActive);
+                setTimeout(function(){ Data.concertsNoResultAllowed = 1; }, 2000);
+          }).
+          error(function(data, status, headers, config) {
+              alert("Concerts list http error: "+status);
+          });
+      } else {
+        console.log("city/1/worksOrderedByFirstPerformance is there locally !!");
+        var worksOrderedByFirstPerformance = localStorage.getItem("city/1/worksOrderedByFirstPerformance");
+        $scope.concerts = JSON.parse(worksOrderedByFirstPerformance);
+        
+      }
+
+
+
+      
       
       $scope.searchActiveToggle = function() {
           Data.searchActive = !Data.searchActive;
           $scope.searchActive = Data.searchActive;
           // console.log("searchActive"+$scope.searchActive);
       };
+
+
  }]);
 /***********************************************************************************************/
 /***********************************************************************************************/
@@ -38,10 +61,7 @@ myApp.controller('ConcertController', ['$rootScope','$scope','$http', '$location
 $scope.colors = ["#fc0003", "#f70008", "#f2000d", "#ed0012", "#e80017", "#e3001c", "#de0021", "#d90026", "#d4002b", "#cf0030", "#c90036", "#c4003b", "#bf0040", "#ba0045", "#b5004a", "#b0004f", "#ab0054", "#a60059", "#a1005e", "#9c0063", "#960069", "#91006e", "#8c0073", "#870078", "#82007d", "#7d0082", "#780087", "#73008c", "#6e0091", "#690096", "#63009c", "#5e00a1", "#5900a6", "#5400ab", "#4f00b0", "#4a00b5", "#4500ba", "#4000bf", "#3b00c4", "#3600c9", "#3000cf", "#2b00d4", "#2600d9", "#2100de", "#1c00e3", "#1700e8", "#1200ed", "#0d00f2", "#0800f7", "#0300fc"];
 
 
-
 $scope.Const = Const;
-
-
 
             $scope.getLandscapeBkgPath = function (object,folder,name_url) {
                 var localImagesPath = "images/";
@@ -134,18 +154,41 @@ $scope.Const = Const;
 
 	var workId = $routeParams.workId;
 
-    $http.get(Const.baseUrl+'/symfony/web/api/work/'+workId).
+//get work
+  if (localStorage.getItem("work/"+workId) === null) {
+      console.log("work not here");
+      $http.get(Const.baseUrl+'/symfony/web/api/work/'+workId).
         success(function(work) {
             Data.headerTitle=work.name;
+            localStorage.setItem("work/"+workId, JSON.stringify(work));
             $scope.getLandscapeBkgPath(work,"works",work.name_url);
         });
+  } else {
+        console.log("work local:");
 
-    $http.get(Const.baseUrl+'/symfony/web/api/work/'+workId+'/performances').
+        var workLS = localStorage.getItem("work/"+workId);
+        workLS = JSON.parse(workLS);
+        Data.headerTitle=workLS.name;
+        $scope.getLandscapeBkgPath(workLS,"works",workLS.name_url);
+  }
+
+
+
+    
+//get performances
+  if (localStorage.getItem('work/'+workId+'/performances') === null) {
+      console.log("performances not here");
+      $http.get(Const.baseUrl+'/symfony/web/api/work/'+workId+'/performances').
         success(function(performances) {
+            localStorage.setItem('work/'+workId+'/performances', JSON.stringify(performances));
             $scope.Data = Data;
             $scope.performances = performances;
         });
-
+  } else {
+        console.log("performances local:");
+        var performancesLS = localStorage.getItem('work/'+workId+'/performances');
+        $scope.performances = JSON.parse(performancesLS);
+  }
 
 
         //swipe
--- v1/angular-static/js/places.js
+++ v1/angular-static/js/places.js
@@ -5,67 +5,117 @@ myApp.controller('PlacesListController', ['$rootScope','$scope','$http','Data','
     Data.headerTitle=Const.appNameFr;
        
        $scope.Data = Data;
-       
-      $http.get(Const.baseUrl+'/symfony/web/api/city/1/places').
-        success(function(places) {            
-            $scope.places = places;
-            Data.loadingActive = 0;             
-        });
+       $scope.Const = Const;
+
+        if (localStorage.getItem("city/1/places") === null) {
+          console.log("places not here");
+          $http.get(Const.baseUrl+'/symfony/web/api/city/1/places').
+            success(function(places) {
+                localStorage.setItem('city/1/places', JSON.stringify(places));
+                $scope.Data = Data;
+                $scope.places = places;
+                Data.loadingActive = 0; 
+            });
+          } else {
+                console.log("places local:");
+                var placesLS = localStorage.getItem('city/1/places');
+                $scope.places = JSON.parse(placesLS);
+                Data.loadingActive = 0; 
+          }
+
  }]);
 
 
+/***************************************************************************/
 
 
 myApp.controller('PlaceController', ['$rootScope','$scope','$http', '$routeParams','Data','Search','Const', function($rootScope, $scope,$http, $routeParams, Data, Search, Const) {
-	// console.log("PC $rootScope.header_title_over = "+$rootScope.header_title_over);
+	
+
+
+  $scope.getLandscapeBkgPath = function (object,folder,name_url) {
+                var localImagesPath = "images/";
+                var serverImagesPath = Const.baseUrl+"/symfony/web/bundles/tapiocadesignclasslivegnv/images/";
+                //works landscape suffix
+                    var pathSuffix = folder+"/"+name_url+"/landscape.jpg";
+                    //if img exist locally, load it, otherwise get in on server
+                    // serverPath
+                    var localImage = localImagesPath+pathSuffix;
+                    $http.get(localImage).
+                    success(function(data, status, headers, config) {
+                        //img exists locally
+                        object.landscapeBkgPath = localImage;
+                        $scope.place = object;
+                         // localImage;
+                    }).
+                    error(function(data, status, headers, config) {
+                        //img does not exist locally, load the distant one
+                        var distantImage = serverImagesPath+pathSuffix
+                         // distantImage;
+                         object.landscapeBkgPath = distantImage;
+                        $scope.place = object;
+                    });
+            };
+
+
     Data.loadingActive = 1;
     Data.concertsNoResultAllowed = 0;
 	var placeId = $routeParams.placeId;
 
-    $http.get(Const.baseUrl+'/symfony/web/api/place/'+placeId).
-        success(function(place) {
-          Data.headerTitle=place.name_fr;
-            $scope.Data = Data;
-            $scope.place = place;
 
-        }).error(function(data, status, headers, config) {
-            //no place found
-        });
+
+
+
+
+
+
+
+
+if (localStorage.getItem('place/'+placeId) === null) {
+          console.log("place not here");
+          $http.get(Const.baseUrl+'/symfony/web/api/place/'+placeId).
+            success(function(place) {
+                localStorage.setItem('place/'+placeId, JSON.stringify(place));
+                $scope.Data = Data;
+                // $scope.place = place;
+                $scope.getLandscapeBkgPath(place,"places",place.name_url);
+                Data.headerTitle=place.name_fr;
+                Data.loadingActive = 0; 
+            });
+          } else {
+                console.log("place local:");
+                var placeLS = localStorage.getItem('place/'+placeId);
+                placeLS = JSON.parse(placeLS);
+                $scope.getLandscapeBkgPath(placeLS,"places",placeLS.name_url);
+                Data.headerTitle=placeLS.name_fr;
+                Data.loadingActive = 0; 
+          }
+
+
+
+
+
+
+
 
         /*********** concerts in this place ***********/
-            $http.get(Const.baseUrl+'/symfony/web/api/place/'+placeId+'/performancesGroupedByWork').
-            success(function(performances) {
-                
-                // if () {
-
-                // }
-                $scope.performances = performances;
-                Data.loadingActive = 0;
-                Data.concertsNoResultAllowed = 1;
-
-                // console.log("Data.loadingActive");
-                // console.log(Data.loadingActive);
-                // console.log("Data.concertsNoResultAllowed");
-                // console.log(Data.concertsNoResultAllowed);
+            if (localStorage.getItem('place/'+placeId+'/performancesGroupedByWork') === null) {
+                console.log("performancesGroupedByWork not here:");
+                $http.get(Const.baseUrl+'/symfony/web/api/place/'+placeId+'/performancesGroupedByWork').
+                success(function(performances) {
+                    localStorage.setItem('place/'+placeId+'/performancesGroupedByWork', JSON.stringify(performances));
+                    $scope.performances = performances;
+                    Data.loadingActive = 0;
+                    Data.concertsNoResultAllowed = 1;
 
             }).error(function(data, status, headers, config) {
-                // console.log("NOTHING");
-                // Data.loadingActive = 0;
-                // Data.concertsNoResultAllowed = 1;
+
               });
-
-  //   $scope.telephone = function(telephone){
-  //   	var link = "tel:"+ telephone; 
-  //   	window.location.href = link;
- 	// };
- 	// $scope.email = function(email){
- 	// 	//console.log("EMAILLLL");
-  //   	var link = "mailto:"+ email; 
-  //   	window.location.href = link;
- 	// };
-    
-
-
-
-
+            } else {
+                console.log("performancesGroupedByWork local:");
+                var performancesLS = localStorage.getItem('place/'+placeId+'/performancesGroupedByWork');
+                performancesLS = JSON.parse(performancesLS);
+                $scope.performances = performancesLS;
+                Data.loadingActive = 0; 
+            }
  }]);
