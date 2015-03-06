6c31f290f6051c0d8d5f83c6641058831872ef6d
Sylvain Hugues-Laclef <info@tapiocadesign.com>
2015-03-06 01:45:54 +0100

    evrthing work but no map

--- v1/phonegap/classlivegnv/platforms/android/ant-build/classes.dex.d
+++ v1/phonegap/classlivegnv/platforms/android/ant-build/classes.dex.d
@@ -6,4 +6,11 @@
 /Users/sylvainhugues/Dropbox/Productions/_PERSO/Classic Live/web/v1/phonegap/classlivegnv/platforms/android/ant-build/classes/com/classlivegnv/tapiocadesign/R$string.class \
 /Users/sylvainhugues/Dropbox/Productions/_PERSO/Classic Live/web/v1/phonegap/classlivegnv/platforms/android/ant-build/classes/com/classlivegnv/tapiocadesign/R$xml.class \
 /Users/sylvainhugues/Dropbox/Productions/_PERSO/Classic Live/web/v1/phonegap/classlivegnv/platforms/android/ant-build/classes/com/classlivegnv/tapiocadesign/R.class \
+/Users/sylvainhugues/Dropbox/Productions/_PERSO/Classic Live/web/v1/phonegap/classlivegnv/platforms/android/ant-build/classes/com/tapiocadesign/classlive/BuildConfig.class \
+/Users/sylvainhugues/Dropbox/Productions/_PERSO/Classic Live/web/v1/phonegap/classlivegnv/platforms/android/ant-build/classes/com/tapiocadesign/classlive/CordovaApp.class \
+/Users/sylvainhugues/Dropbox/Productions/_PERSO/Classic Live/web/v1/phonegap/classlivegnv/platforms/android/ant-build/classes/com/tapiocadesign/classlive/R$attr.class \
+/Users/sylvainhugues/Dropbox/Productions/_PERSO/Classic Live/web/v1/phonegap/classlivegnv/platforms/android/ant-build/classes/com/tapiocadesign/classlive/R$drawable.class \
+/Users/sylvainhugues/Dropbox/Productions/_PERSO/Classic Live/web/v1/phonegap/classlivegnv/platforms/android/ant-build/classes/com/tapiocadesign/classlive/R$string.class \
+/Users/sylvainhugues/Dropbox/Productions/_PERSO/Classic Live/web/v1/phonegap/classlivegnv/platforms/android/ant-build/classes/com/tapiocadesign/classlive/R$xml.class \
+/Users/sylvainhugues/Dropbox/Productions/_PERSO/Classic Live/web/v1/phonegap/classlivegnv/platforms/android/ant-build/classes/com/tapiocadesign/classlive/R.class \
 /Users/sylvainhugues/Dropbox/Productions/_PERSO/Classic Live/web/v1/phonegap/classlivegnv/platforms/android/ant-build/dexedLibs/classes-4e8daf06b50721be81bd9f2524180792.jar \
---
+++ v1/phonegap/classlivegnv_works/platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/sylvainhugues/Dropbox/Productions/_PERSO/Classic Live/web/v1/phonegap/classlivegnv_works/platforms/android/ant-build/AndroidManifest.xml : \
+/Users/sylvainhugues/Dropbox/Productions/_PERSO/Classic Live/web/v1/phonegap/classlivegnv_works/platforms/android/AndroidManifest.xml \
