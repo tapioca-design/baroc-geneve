a250e2d8b30e3da0aa00e1d28edee9ca40d3f21e
Sylvain Hugues-Laclef <info@tapiocadesign.com>
2015-03-06 01:23:20 +0100

    remis sur pieds

---
+++ v1/phonegap/classlivegnv/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/sylvainhugues/Dropbox/Productions/_PERSO/Classic Live/web/v1/phonegap/classlivegnv/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
+/Users/sylvainhugues/Dropbox/Productions/_PERSO/Classic Live/web/v1/phonegap/classlivegnv/platforms/android/CordovaLib/AndroidManifest.xml \
---
+++ v1/phonegap/classlivegnv/platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,9 @@
+/Users/sylvainhugues/Dropbox/Productions/_PERSO/Classic Live/web/v1/phonegap/classlivegnv/platforms/android/ant-build/classes.dex : \
+/Users/sylvainhugues/Dropbox/Productions/_PERSO/Classic Live/web/v1/phonegap/classlivegnv/platforms/android/ant-build/classes/com/classlivegnv/tapiocadesign/BuildConfig.class \
+/Users/sylvainhugues/Dropbox/Productions/_PERSO/Classic Live/web/v1/phonegap/classlivegnv/platforms/android/ant-build/classes/com/classlivegnv/tapiocadesign/CordovaApp.class \
+/Users/sylvainhugues/Dropbox/Productions/_PERSO/Classic Live/web/v1/phonegap/classlivegnv/platforms/android/ant-build/classes/com/classlivegnv/tapiocadesign/R$attr.class \
+/Users/sylvainhugues/Dropbox/Productions/_PERSO/Classic Live/web/v1/phonegap/classlivegnv/platforms/android/ant-build/classes/com/classlivegnv/tapiocadesign/R$drawable.class \
+/Users/sylvainhugues/Dropbox/Productions/_PERSO/Classic Live/web/v1/phonegap/classlivegnv/platforms/android/ant-build/classes/com/classlivegnv/tapiocadesign/R$string.class \
+/Users/sylvainhugues/Dropbox/Productions/_PERSO/Classic Live/web/v1/phonegap/classlivegnv/platforms/android/ant-build/classes/com/classlivegnv/tapiocadesign/R$xml.class \
+/Users/sylvainhugues/Dropbox/Productions/_PERSO/Classic Live/web/v1/phonegap/classlivegnv/platforms/android/ant-build/classes/com/classlivegnv/tapiocadesign/R.class \
+/Users/sylvainhugues/Dropbox/Productions/_PERSO/Classic Live/web/v1/phonegap/classlivegnv/platforms/android/ant-build/dexedLibs/classes-4e8daf06b50721be81bd9f2524180792.jar \
