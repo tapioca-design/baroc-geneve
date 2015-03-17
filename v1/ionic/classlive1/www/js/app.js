6f3ea802b81377e62d4ee9335b85300e64c6c582
Sylvain Hugues-Laclef <info@tapiocadesign.com>
2015-03-17 23:46:18 +0100

    one step forward

--- v1/ionic/classlive1/www/js/app.js
+++ v1/ionic/classlive1/www/js/app.js
@@ -1,12 +1,4 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
 var starter = angular.module('starter', ['ionic', "ngRoute", "ngTouch","ngResource","ngAnimate", 'angular-carousel']);
-
 
 starter.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -22,19 +14,73 @@ starter.run(function($ionicPlatform) {
   });
 })
 
+// starter.controller('DemoCtrl', function($scope) {
+
+//             $scope.colors = ["#fc0003", "#f70008", "#f2000d", "#ed0012", "#e80017", "#e3001c", "#de0021", "#d90026", "#d4002b", "#cf0030", "#c90036", "#c4003b", "#bf0040", "#ba0045", "#b5004a", "#b0004f", "#ab0054", "#a60059", "#a1005e", "#9c0063", "#960069", "#91006e", "#8c0073", "#870078", "#82007d", "#7d0082", "#780087", "#73008c", "#6e0091", "#690096", "#63009c", "#5e00a1", "#5900a6", "#5400ab", "#4f00b0", "#4a00b5", "#4500ba", "#4000bf", "#3b00c4", "#3600c9", "#3000cf", "#2b00d4", "#2600d9", "#2100de", "#1c00e3", "#1700e8", "#1200ed", "#0d00f2", "#0800f7", "#0300fc"];
+
+//             function getSlide(target, style) {
+//                 var i = target.length;
+//                 return {
+//                     id: (i + 1),
+//                     label: 'slide #' + (i + 1),
+//                     img: 'http://lorempixel.com/450/300/' + style + '/' + ((i + 1) % 10) ,
+//                     color: $scope.colors[ (i*10) % $scope.colors.length],
+//                     odd: (i % 2 === 0)
+//                 };
+//             }
+
+//             function addSlide(target, style) {
+//                 target.push(getSlide(target, style));
+//             };
+
+//             $scope.carouselIndex = 3;
+//             $scope.carouselIndex2 = 0;
+//             $scope.carouselIndex2 = 1;
+//             $scope.carouselIndex3 = 5;
+//             $scope.carouselIndex4 = 5;
+
+//             function addSlides(target, style, qty) {
+//                 for (var i=0; i < qty; i++) {
+//                     addSlide(target, style);
+//                 }
+//             }
+
+//             // 1st ngRepeat demo
+//             $scope.slides = [];
+//             addSlides($scope.slides, 'sports', 50);
+
+//             // 2nd ngRepeat demo
+//             $scope.slides2 = [];
+//             addSlides($scope.slides2, 'sports', 10);
+
+//             // 3rd ngRepeat demo
+//             $scope.slides3 = [];
+//             addSlides($scope.slides3, 'people', 50);
+
+//             // 4th ngRepeat demo
+//             $scope.slides4 = [];
+//             addSlides($scope.slides4, 'city', 50);
+
+
+//             // 5th ngRepeat demo
+//             $scope.slides6 = [];
+//             $scope.carouselIndex6 = 0;
+//             addSlides($scope.slides6, 'sports', 10);
+//             $scope.addSlide = function(at) {
+//                 if(at==='head') {
+//                     $scope.slides6.unshift(getSlide($scope.slides6, 'people'));
+//                 } else {
+//                     $scope.slides6.push(getSlide($scope.slides6, 'people'));
+//                 }
+//             }
+
+//         });
+
 starter.config(['$httpProvider', function($httpProvider) {
         $httpProvider.defaults.useXDomain = true;
         delete $httpProvider.defaults.headers.common['X-Requested-With'];
     }
 ]);
-
-
-
-
-
-
-
-
 
 starter.run(function ($rootScope, $location,$http,Const,Data) {
 
@@ -84,6 +130,7 @@ starter.run(function ($rootScope, $location,$http,Const,Data) {
             var url=Const.baseUrl+'/symfony/web/api/'+url_suffix;
             $http.get(url).
             success(function(data) {
+              // $rootScope.d("http succes for "+url);
                 localStorage.setItem(url_suffix, JSON.stringify(data));
                 //if no need to add landscape img to data, skip
                 if (folder!="") {
@@ -146,19 +193,6 @@ starter.factory('Search', function(){
 starter.factory('Concert', function(){
     return {term: ''};
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 starter.config(function($stateProvider, $urlRouterProvider) {
 
--- v1/ionic/classlive1/www/js/concerts.js
+++ v1/ionic/classlive1/www/js/concerts.js
@@ -10,33 +10,14 @@ starter.controller('ConcertsCtrl', ['$rootScope' ,'$scope', function($rootScope,
 }]);
 
 
-starter.controller('ConcertDetailCtrl', ['$rootScope', '$scope', '$stateParams', function($rootScope, $scope, $stateParams) {
-          // $scope.concert = Concerts.get($stateParams.concertId);
-
-          // $scope.test = "zuzu";
-
-          var workId = $stateParams.workId;
-          // $rootScope.d("workId"+workId);
-
-          $rootScope.getData("work/"+workId, "works",
-          function (callback_arg) {
-              var work = callback_arg;
-              // Data.headerTitle=work.name;
-              // $scope.Data = Data;
-              $scope.workTitle = work.name;
-              $scope.work = work;
-          });
-          $rootScope.getData('work/'+workId+'/performances', "works",
-          function (callback_arg) {
-              var performances = callback_arg;
-              // $scope.Data = Data;
-              $scope.performances = performances;
-          });
+starter.controller('ConcertDetailCtrl', ['$rootScope', '$scope', '$stateParams','$location', function($rootScope, $scope, $stateParams,$location) {
+          
           $scope.colors = ["#fc0003", "#f70008", "#f2000d", "#ed0012", "#e80017", "#e3001c", "#de0021", "#d90026", "#d4002b", "#cf0030", "#c90036", "#c4003b", "#bf0040", "#ba0045", "#b5004a", "#b0004f", "#ab0054", "#a60059", "#a1005e", "#9c0063", "#960069", "#91006e", "#8c0073", "#870078", "#82007d", "#7d0082", "#780087", "#73008c", "#6e0091", "#690096", "#63009c", "#5e00a1", "#5900a6", "#5400ab", "#4f00b0", "#4a00b5", "#4500ba", "#4000bf", "#3b00c4", "#3600c9", "#3000cf", "#2b00d4", "#2600d9", "#2100de", "#1c00e3", "#1700e8", "#1200ed", "#0d00f2", "#0800f7", "#0300fc"];
 
             $scope.path = function ( path ) {
               // console.log("path="+path);
-              $location.url(path);
+              $rootScope.d("path "+path);
+              $location.path(path);
               
             };
             $scope.absolute = function ( path ) {
@@ -79,6 +60,28 @@ starter.controller('ConcertDetailCtrl', ['$rootScope', '$scope', '$stateParams',
                     $scope.slides6.push(getSlide($scope.slides6, 'people'));
                 }
             }
+
+            // $scope.concert = Concerts.get($stateParams.concertId);
+
+          // $scope.test = "zuzu";
+
+          var workId = $stateParams.workId;
+          // $rootScope.d("workId"+workId);
+
+          $rootScope.getData("work/"+workId, "works",
+          function (callback_arg) {
+              var work = callback_arg;
+              // Data.headerTitle=work.name;
+              // $scope.Data = Data;
+              $scope.workTitle = work.name;
+              $scope.work = work;
+          });
+          $rootScope.getData('work/'+workId+'/performances', "works",
+          function (callback_arg) {
+              var performances = callback_arg;
+              // $scope.Data = Data;
+              $scope.performances = performances;
+          });
 }]);
 
 
