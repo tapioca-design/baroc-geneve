cace0702fde8fe45ce67916fbbc9ff75011a63ea
Sylvain Hugues-Laclef <info@tapiocadesign.com>
2015-04-28 03:18:59 +0200

    about to apply http://bl.ocks.org/marcoslin/df4b741e92b2829eeae8

---
+++ v1/ionic/classlive4/www/js/main.js
@@ -0,0 +1,10 @@
+require.config({
+    baseUrl: "js",    
+    paths: {
+        'angular': '.../angular.min',
+        'angular-route': '.../angular-route.min',
+        'angularAMD': '.../angularAMD.min'
+    },
+    shim: { 'angularAMD': ['angular'], 'angular-route': ['angular'] },
+    deps: ['app']
+});
